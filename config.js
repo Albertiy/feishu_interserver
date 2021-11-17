4815fe32a1cbaf47426cd6b4b5a2a4a615620dab
Albertiy <YUANYUE963@outlook.com>
2021-11-17 13:59:48 +0800

    服务器基本框架搭建完毕

---
+++ config.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+/**
+ * 获取 application.config.json
+ * @returns {{
+ * port: number,
+ * env: string,
+ * staticPath: string,
+ * logDirectory: string,
+ * }}
+ */
+exports.application = function () {
+    try {
+        return JSON.parse(fs.readFileSync('./config/application.config.json'));
+    } catch (_) {
+        return {};
+    }
+}
+
+/**
+ * 获取 database.config.json
+ * @returns {{
+ * }}
+ */
+exports.database = function () {
+    try {
+        return JSON.parse(fs.readFileSync('./config/database.config.json'));
+    } catch (_) {
+        return {};
+    }
+}
