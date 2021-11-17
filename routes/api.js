56d8977d597221ae2594b5f71927fb238e57061b
Albertiy <YUANYUE963@outlook.com>
2021-11-17 14:17:53 +0800

    添加验证飞书订阅验证API

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 module.exports = router;
+
+router.post('/subscribe', function (req, res, next) {
+  /**
+   * @type {{
+   *  challenge: string,
+   *  token: string,
+   *  type: string
+   * }}
+   */
+  let data = req.body;
+  res.json(data?.challenge)
+})
